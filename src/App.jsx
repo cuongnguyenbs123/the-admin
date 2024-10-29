2c2637a3f0286739db314b7004b891f8a7853776
Dai Cuong <cuong2000bs@gmail.com>
2024-10-29 17:04:07 +0700

    chore: add react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import './App.css'
-function App() {
-  return (
-    <>
-      <div>Dashboard Admin</div>
-    </>
-  )
-}
+import { BrowserRouter, Routes, Route, Navigate  } from "react-router-dom";
+import LoginLayout from "@/layout/Login";
+import Home from "@/pages/Home";
 
-export default App
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginLayout />}>
+        </Route>
+      </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Navigate to="/" replace />}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
